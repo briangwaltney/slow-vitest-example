29134d5a9ef8ac72820e8b99f44f7cc7717d5b68
Brian Gwaltney <brian.gwaltney@gmail.com>
2023-01-09 15:08:36 +0300

    included wallaby diagnostics

--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { testFunc } from "./index";
+import { testFunc1 } from "./index";
 
 describe("index", () => {
   it("testFunc1", () => {
-    expect(testFunc(1, 2)).toBe(3);
+    expect(testFunc1(1, 2)).toBe(3);
     expect(1).toBe(1);
   });
 });
